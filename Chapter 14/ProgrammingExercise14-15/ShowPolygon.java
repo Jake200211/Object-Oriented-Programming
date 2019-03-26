b52f2a13fce1ef6a3d7d8f2eb276b7f88c2d228e
Jake200211 <42577458+Jake200211@users.noreply.github.com>
2019-03-26 07:22:36 -0600

    Add files via upload

---
+++ Chapter 14/ProgrammingExercise14-15/ShowPolygon.java
@@ -0,0 +1,73 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.*;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javafx.collections.ObservableList;
+import javafx.scene.shape.Polygon;
+import javafx.scene.layout.StackPane;
+
+public class ShowPolygon extends Application {
+	@Override // Override the start method in the Application class
+	public void start(Stage primaryStage) {   
+		// Create a scene and place it in the stage
+		Scene scene = new Scene(new MyPolygon(), 400, 400);
+		primaryStage.setTitle("ShowPolygon"); // Set the stage title
+		primaryStage.setScene(scene); // Place the scene in the stage
+		primaryStage.show(); // Display the stage
+	}
+	
+	/**
+	 * The main method is only needed for the IDE with limited
+	 * JavaFX support. Not needed for running from the command line.
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
+
+class MyPolygon extends StackPane {
+	private void paint() {
+		// Create a polygon and place polygon to pane
+		Polygon polygon = new Polygon();
+		polygon.setFill(Color.RED);
+		polygon.setStroke(Color.WHITE);
+		ObservableList<Double> list = polygon.getPoints();
+		
+		double centerX = getWidth() / 2, centerY = getHeight() / 2;
+		double radius = Math.min(getWidth(), getHeight()) * 0.4;
+
+		// s represents the number of sides of the shape
+		// Make sure to update this number when necessary
+		int s = 8;
+		// Add points to the polygon list
+		for (int i = 0; i < s; i++) {
+			list.add(centerX + radius * Math.cos(2 * i * Math.PI / s)); 
+			list.add(centerY - radius * Math.sin(2 * i * Math.PI / s));
+			polygon.setRotate(22.5);
+		}    
+				
+		getChildren().clear();
+		getChildren().add(polygon); 
+		
+		Pane pane1 = new StackPane();
+		Label label = new Label("STOP");
+		label.setFont(Font.font("Times New Roman", 
+			FontWeight.BOLD, 50));
+		getChildren().add(label);
+	}
+	
+	@Override
+	public void setWidth(double width) {
+		super.setWidth(width);
+		paint();
+	}
+	
+	@Override
+	public void setHeight(double height) {
+		super.setHeight(height);
+		paint();
+	}
+}
